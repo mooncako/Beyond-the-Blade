bbe49988a8280069c530f3e8c559c69c8314739a
Pat Wang <mooncako@gmail.com>
2025-08-15 15:54:35 -0700

    Start working on enemy AI

---
+++ Assets/Scripts/GOAP/Goals/IdleGoal.cs
@@ -0,0 +1,9 @@
+using CrashKonijn.Goap.Runtime;
+
+namespace CrashKonijn.Goap.GenTest
+{
+    [GoapId("Idle-6731a1f6-0d10-49c0-baef-88ba639d967a")]
+    public class IdleGoal : GoalBase
+    {
+    }
+}
