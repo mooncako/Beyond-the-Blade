62c9e3b503f033782bdfbca9f0929646a0f6ace4
Pat Wang <mooncako@gmail.com>
2025-08-19 22:07:29 -0700

    Start of AnimationStateMachine

--- Assets/Scripts/Character/Controller/EnemyController.cs
+++ Assets/Scripts/Character/Controller/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Animancer;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityUtils;
@@ -69,9 +70,37 @@ public class EnemyController : Controller, IPoolable
         Movement.Stop();
     }
 
+    public override void DamageAnimEvent()
+    {
+        if (_currentSkill.IsTargetedGroundAOE)
+        {
+            _hitTargets = AOEApplier.GetDamagedEntities(_currentSkill.SkillRange.AreaType, _targetPos);
+        }
+        else
+        {
+            if (AttackPoint != null)
+            {
+                _hitTargets = AOEApplier.GetDamagedEntities(_currentSkill.SkillRange.AreaType, AttackPoint.position);
+            }
+            else
+            {
+                _hitTargets = AOEApplier.GetDamagedEntities(_currentSkill.SkillRange.AreaType, transform.position);
+            }
+        }
+
+
+        foreach (GameObject target in _hitTargets)
+        {
+            DamageInfo info = new DamageInfo(_currentSkill.Damage, target, gameObject, gameObject, DamageType.Regular);
+            target.GetComponent<Health>().Damage(info);
+        }
+
+        _isSkillPlaying = false;
+    }
+
     public void OnPoolGet()
     {
-        
+
     }
 
     public void OnPoolReturn()
--- Assets/Scripts/Combat/AIUtil.cs
+++ Assets/Scripts/Combat/AIUtil.cs
@@ -6,11 +6,11 @@ public static class AIUtil
     public static Vector3 GetRandomSpawnPos(NavMeshTriangulation triangulation)
     {
         NavMeshHit hit;
-        
+
         int vertexIndex = Random.Range(0, triangulation.vertices.Length);
         for (int i = 0; i < 30; i++)
         {
-            if (NavMesh.SamplePosition(triangulation.vertices[vertexIndex], out hit, 2, NavMesh.AllAreas))
+            if (NavMesh.SamplePosition(triangulation.vertices[vertexIndex], out hit, 50, NavMesh.AllAreas))
             {
                 return hit.position;
             }
