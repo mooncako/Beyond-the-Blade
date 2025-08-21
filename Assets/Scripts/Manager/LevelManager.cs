211c863fb270bda16fcb7f2e8946d29e8c3b1d97
Pat Wang <mooncako@gmail.com>
2025-08-21 16:09:27 -0700

    Start working on level generation and related tools

---
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,15 @@
+using MoreMountains.Tools;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class LevelManager : MMSingleton<LevelManager>
+{
+    [SerializeField, BoxGroup("References")] private LevelSystem[] _availableLevelPrefabs;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        DontDestroyOnLoad(this);
+    }
+}
