9bf4ae12b5d60ddf9718d93be355fb404f408785
Pat Wang <mooncako@gmail.com>
2025-08-20 20:27:50 -0700

    Update spawnlocation algorithm

--- Assets/Scripts/Data/GameDifficultyDataSO.cs
+++ Assets/Scripts/Data/GameDifficultyDataSO.cs
@@ -5,7 +5,7 @@ public class GameDifficultyDataSO : ScriptableObject
 {
     [SerializeField] public float TotalLevelCount = 15;
     [SerializeField] public int StartingWaveBudget = 10;
-    [field: SerializeField] public float BudgetScale = 1.5f;
+    [field: SerializeField] public float BudgetScale = 1.15f;
     [field: SerializeField] public AnimationCurve MinDifficultyCurve { get; private set; }
     [field: SerializeField] public AnimationCurve MaxDifficultyCurve { get; private set; }
 }
