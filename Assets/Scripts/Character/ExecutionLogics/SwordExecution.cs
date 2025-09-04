106ef3eda7dde73117a53afa2cbd8b822f504ef3
mooncako <mooncako@gmail.com>
2025-09-04 15:27:07 +0800

    Added note for todo

--- Assets/Scripts/Character/ExecutionLogics/SwordExecution.cs
+++ Assets/Scripts/Character/ExecutionLogics/SwordExecution.cs
@@ -5,6 +5,6 @@ public class SwordExecution : Execution
     public override void Perform()
     {
         base.Perform();
-        
+        //TODO: Teleport to target location and stagger the enemies in range
     }
 }
