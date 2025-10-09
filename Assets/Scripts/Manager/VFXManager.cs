433e2bfa0ea05f1dd66f1fb74eb3038147e8582a
mooncako <mooncako@gmail.com>
2025-10-09 21:40:28 +0800

    Turn vfx manager into a prefab, add another way to spawn vfx when there is no transform input

--- Assets/Scripts/Manager/VFXManager.cs
+++ Assets/Scripts/Manager/VFXManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.VFX;
 
 [RequireComponent(typeof(ObjectPool))]
+[RequireComponent(typeof(DontDestroy))]
 public class VFXManager : MonoBehaviour,
     MMEventListener<SpawnVFXEvent>
 {
@@ -38,9 +39,17 @@ public class VFXManager : MonoBehaviour,
         if (_prefabDatabase.VFXDatabase.ContainsKey(e.Id))
         {
             GameObject go = _pool.Get(_prefabDatabase.VFXDatabase[e.Id]);
-            go.transform.SetParent(e.Owner, false);
-            go.transform.position = e.Owner.position;
-            go.transform.localPosition = new Vector3(go.transform.localPosition.x + e.Info.Pos.x, go.transform.localPosition.y + e.Info.Pos.y, go.transform.localPosition.z + e.Info.Pos.z);
+            if (e.Owner != null)
+            {
+                go.transform.SetParent(e.Owner, false);
+                go.transform.position = e.Owner.position;
+                go.transform.localPosition = new Vector3(go.transform.localPosition.x + e.Info.Pos.x, go.transform.localPosition.y + e.Info.Pos.y, go.transform.localPosition.z + e.Info.Pos.z);
+            }
+            else
+            {
+                go.transform.position = e.Info.Pos;
+            }
+            
             go.transform.rotation = e.Info.Rot;
             go.transform.localScale = e.Info.Scale;
             go.GetComponent<VisualEffect>().Play();
