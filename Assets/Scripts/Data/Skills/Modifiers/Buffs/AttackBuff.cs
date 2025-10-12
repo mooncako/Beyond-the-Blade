bf518afef2a13ab2d47724db935799d054df5404
Xwen66 <xwenx66@gmail.com>
2025-10-11 17:54:25 -0700

    added dash and attack buff

---
+++ Assets/Scripts/Data/Skills/Modifiers/Buffs/AttackBuff.cs
@@ -0,0 +1,36 @@
+using PrimeTween;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Attack", menuName = "Modifier/Buffs/AttackBuff")]
+public class AttackBuff : ModifierSO
+{
+    //double the attack for 3 seconds
+    public float DamageMultiplierIncrease = 1f;
+    public float Duration = 3f;
+
+    public override void Perform(in ModifierContext context)
+    {
+        if (context.Caster is PlayerController p)
+        {
+            ApplyAttackBuff(p);
+        }
+        else if (context.Caster is EnemyController e)
+        {
+            ApplyAttackBuff(e);
+        }
+    }
+
+    private void ApplyAttackBuff(Controller controller)
+    {
+        controller.Stats.TempDamageMultiplier += DamageMultiplierIncrease;
+        controller.OnStatsUpdated();
+        Tween.Delay(Duration).OnComplete(() =>
+        {
+            if (controller != null && controller.Stats != null)
+            {
+                controller.Stats.TempDamageMultiplier -= DamageMultiplierIncrease;
+                controller.OnStatsUpdated();
+            }
+        });
+    }
+}
