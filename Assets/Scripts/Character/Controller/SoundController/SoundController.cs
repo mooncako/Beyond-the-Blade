35690a5eb807a1d7bbe4e409a71cf87c60e580ca
Xwen66 <xwenx66@gmail.com>
2025-10-14 13:05:30 -0700

    added enemies onHitSfx

--- Assets/Scripts/Character/Controller/SoundController/SoundController.cs
+++ Assets/Scripts/Character/Controller/SoundController/SoundController.cs
@@ -6,6 +6,30 @@ public class SoundController : MonoBehaviour
     [SerializeField] protected FMODUnity.EventReference _attackSFX;
     [SerializeField] protected FMODUnity.EventReference _footstepSFX;
     [SerializeField] protected FMODUnity.EventReference _movementSFX;
+    [SerializeField] protected FMODUnity.EventReference _onHitSFX;
+    [SerializeField] protected FMODUnity.EventReference _onSpawnSFX;
+    [SerializeField] protected FMODUnity.EventReference _onDeathSFX;
+    [SerializeField] protected FMODUnity.EventReference _dissolveDeathSFX;
+
+    [Header("References")]
+    [SerializeField] protected Health _health;
+
+    void OnValidate()
+    {
+        if (_health == null) _health = GetComponent<Health>();
+    }
+
+    void OnEnable()
+    {
+        _health.OnDamage.AddListener(PlayOnHitSFX);
+        _health.OnDeath.AddListener(PlayOnDeathSFX);
+    }
+
+    void OnDisable()
+    {
+        _health.OnDamage.RemoveListener(PlayOnHitSFX);
+        _health.OnDeath.RemoveListener(PlayOnDeathSFX);
+    }
 
     public virtual void PlayFootstep()
     {
@@ -20,5 +44,25 @@ public class SoundController : MonoBehaviour
     public virtual void PlayAttackSFX()
     {
 
+    }
+
+    public virtual void PlayOnHitSFX(DamageInfo info)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(_onHitSFX, transform.position);
+    }
+
+    public virtual void PlayOnSpawnSFX()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(_onSpawnSFX, transform.position);
+    }
+
+    public virtual void PlayOnDeathSFX(DamageInfo info)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(_onDeathSFX, transform.position);
+    }
+
+    public virtual void PlayDissolveDeathSFX(DamageInfo info)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(_dissolveDeathSFX, transform.position);
     }
 }
