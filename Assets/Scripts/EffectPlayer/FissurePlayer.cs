1f6084775139997a2d46d19ed82b8752bed7c40a
Pat Wang <mooncako@gmail.com>
2025-11-11 13:30:57 -0800

    Add fissure close vfx

--- Assets/Scripts/EffectPlayer/FissurePlayer.cs
+++ Assets/Scripts/EffectPlayer/FissurePlayer.cs
@@ -1,10 +1,12 @@
+using MoreMountains.Tools;
 using PrimeTween;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.VFX;
 
-public class FissurePlayer : MonoBehaviour
+public class FissurePlayer : MonoBehaviour,
+    MMEventListener<ProgressionCanvasCloseEvent>
 {
     [SerializeField, BoxGroup("References")] private ParticleSystem _fissureFront;
     [SerializeField, BoxGroup("References")] private ParticleSystem _fissureBack;
@@ -19,11 +21,13 @@ public class FissurePlayer : MonoBehaviour
     [SerializeField, BoxGroup("Settings")] private AnimationCurve _fissureProgressCurve;
     [SerializeField, BoxGroup("Settings")] private AnimationCurve _crackProgressCurve;
     [SerializeField, BoxGroup("Settings")] private float _fissureOpenTime = .8f;
+    [SerializeField, BoxGroup("Settings")] private float _fissureCloseTime = 1.5f;
     [SerializeField, BoxGroup("Settings")] private float _colorChangeTime = 2f;
     [SerializeField, BoxGroup("Settings")] private AnimationCurve _screenCrackEdgeCurve;
     [SerializeField, BoxGroup("Settings")] private AnimationCurve _screenCrackOffsetCurve;
     [SerializeField, BoxGroup("Settings"), ColorUsage(true, true)] private Color _backMatStartColor;
     [SerializeField, BoxGroup("Settings"), ColorUsage(true, true)] private Color _backMatEndColor;
+    [SerializeField, BoxGroup("Settings")] private AnimationCurve _innerEdgeCurve;
 
     [HideInInspector] public UnityEvent OnFissureOpen;
 
@@ -32,8 +36,12 @@ public class FissurePlayer : MonoBehaviour
     private Tween _backMatColorTween;
     private Tween _delayTween1;
     private Tween _delayTween2;
+    private Tween _closureTween;
 
-
+    void OnEnable()
+    {
+        this.MMEventStartListening<ProgressionCanvasCloseEvent>();
+    }
 
     void OnDisable()
     {
@@ -42,11 +50,13 @@ public class FissurePlayer : MonoBehaviour
         _backMatColorTween.Stop();
         _delayTween1.Stop();
         _delayTween2.Stop();
+        this.MMEventStopListening<ProgressionCanvasCloseEvent>();
     }
 
     [Button]
     public void Play()
     {
+        _backRenderer.material.SetFloat("_InnerEdge", 0);
         _fissureBack.Stop();
         _fissureFront.Stop();
         _fissureCrack.Stop();
@@ -72,7 +82,7 @@ public class FissurePlayer : MonoBehaviour
         });
 
         _delayTween1 = Tween.Delay(_fissureOpenTime / 3).OnComplete(() => _fracture.Play());
-        
+
         _delayTween2 = Tween.Delay(_fissureOpenTime / 2).OnComplete(() =>
         {
             _screenCrackTween = Tween.Custom(0, 1, duration: _fissureOpenTime, onValueChange: prog =>
@@ -85,6 +95,18 @@ public class FissurePlayer : MonoBehaviour
                 _backRenderer.material.SetColor("_VoidColor", Color.Lerp(_backMatStartColor, _backMatEndColor, prog));
             });
         });
-        
+
+    }
+    
+    [Button]
+    public void Close()
+    {
+        _closureTween.Stop();
+        _closureTween = Tween.Custom(0, 1, duration: _fissureCloseTime, onValueChange: prog => _backRenderer.material.SetFloat("_InnerEdge", _innerEdgeCurve.Evaluate(prog)));
+    }
+
+    public void OnMMEvent(ProgressionCanvasCloseEvent e)
+    {
+        Close();
     }
 }
