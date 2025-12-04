e18bd061a148e047983b7856f5cbab398f6a4279
Pat Wang <mooncako@gmail.com>
2025-12-04 13:33:01 -0800

    Intializing the combat changes

--- Assets/Scripts/Character/Posture.cs
+++ Assets/Scripts/Character/Posture.cs
@@ -8,9 +8,11 @@ public class Posture : MonoBehaviour
 {
     [Title("Stun Progress", titleAlignment: TitleAlignments.Centered)]
     [ProgressBar(0, "StunThreshold", ColorGetter = "GetStunThresholdColor", Height = 30), HideLabel] public float CurrentStunValue = 0;
+    public float StunPercentile => CurrentStunValue / StunThreshold;
     [SerializeField, BoxGroup("Debug"), ReadOnly] private bool _stunned = false;
     [HideInInspector] public float StunThreshold;
     [HideInInspector] public UnityEvent<float> OnStunned; 
+    [HideInInspector] public UnityEvent<float> OnStunUpdate;
 
 
     public void ApplyStats(float stunThreshold)
@@ -23,6 +25,7 @@ public class Posture : MonoBehaviour
         if(_stunned) return;
 
         CurrentStunValue = Mathf.Clamp(CurrentStunValue + value, 0, StunThreshold);
+        OnStunUpdate.Invoke(StunPercentile);
         if(CurrentStunValue.Approx(StunThreshold))
         {
             OnStunned.Invoke(duration);
@@ -43,6 +46,7 @@ public class Posture : MonoBehaviour
         {
             yield return new WaitForSeconds(.1f);
             CurrentStunValue = Mathf.Clamp(CurrentStunValue - StunThreshold/(duration/.1f), 0, StunThreshold);
+            OnStunUpdate.Invoke(StunPercentile);
         }
         _stunned = false;
     }
---
+++ Assets/Scripts/Data/SkillAnimation.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using Animancer;
+using UnityEngine;
+
+[Serializable]
+public class SkillAnimation
+{
+    public List<ClipTransition> Animations = new List<ClipTransition>();
+}
